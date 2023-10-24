eafa27d72a6d55c3693e09da2d055c2b0e16ddb5
Your Name <you@example.com>
2023-10-24 06:26:50 +0530

    added same tsconfig

--- ipcserver/bin/ipc.ts
+++
@@ -1,8 +0,0 @@
-//#!/usr/bin/env node
-import * as path from 'path';
-// capture the bin folder to be used as current working directory across the board
-const cwd:string = path.resolve(__dirname) || process.cwd()
-process.env.ipcCWD = cwd
-const ipcTitle = 'ETP-IPCHost',
-process.title = ipcTitle
-require("../lib/init")
