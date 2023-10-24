3fa777d396f26c16ba68f2cee851339af65ff090
Your Name <you@example.com>
2023-10-24 06:25:47 +0530

    added same tsconfig

--- etp-ipchost/bin/ipc.ts
+++ etp-ipchost/bin/ipc.ts
@@ -3,6 +3,6 @@ import * as path from 'path';
 // capture the bin folder to be used as current working directory across the board
 const cwd:string = path.resolve(__dirname) || process.cwd()
 process.env.ipcCWD = cwd
-const ipcTitle = 'ETP-IPCHost',
+const ipcTitle = 'ETP-IPCHost';
 process.title = ipcTitle
-require("../lib/init")
+require('../lib/init')
