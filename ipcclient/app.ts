639b22f3779db2c591dc3ef5eb053420829675a2
Your Name <you@example.com>
2023-11-01 19:19:54 +0530

    added ipccontrol constructer

--- ipcclient/app.ts
+++ ipcclient/app.ts
@@ -1,7 +1,10 @@
+import { IpcControlImpl } from './ipc.controls';
+import { IpcControl } from './ipc.interface';
 
 
 
 const initApp = async() => {
   console.log("initApp  called")
+  let IpcControl = new IpcControlImpl(true)
 }
 initApp()
