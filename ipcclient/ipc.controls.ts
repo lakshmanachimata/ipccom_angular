6f32d5405e9330cf06b6c814d522f40431434d74
Your Name <you@example.com>
2023-11-01 08:38:14 +0530

    added const related changes to app config

---
+++ ipcclient/ipc.controls.ts
@@ -0,0 +1,8 @@
+import { Observable,Subject } from "rxjs";
+import { IpcControl,IpcEvent,DFContext,DFOptions,DFNavigatorQueryPair } from "./ipc.interface";
+import {filter, map} from 'rxjs/operators'
+
+export class IpcControlImpl implements IpcControl {
+
+
+}
---
+++ ipcclient/ipc.interface.ts
@@ -0,0 +1,37 @@
+import { Observable } from 'rxjs';
+
+export interface IpcEvent {
+  name: string,
+  value: any
+}
+
+export interface DFNavigatorQueryPair {
+  name: string,
+  value: string
+}
+
+export interface DFOptions {
+ appId: string,
+ title: string,
+ containerId: string
+}
+
+export interface DFContext {
+  key: string,
+  value: string,
+  child?: DFContext[]
+}
+
+export interface IpcControl {
+
+  dispose(): void;
+
+  navigateTo(options: DFOptions, queryPair: DFNavigatorQueryPair[]): void;
+  navigateToContainer(options: DFOptions, queryPair: DFNavigatorQueryPair[]): void;
+
+  getContext(key: string): Observable<any>;
+  setContext(context: DFContext): any;
+
+  publish(eventName: string,eventData: string | Object): void;
+  subscibe(eventName: string): Observable<any>;
+}
